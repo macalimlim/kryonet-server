822fcdf15d2b8f21802eb254134721b13a02c9e5
macalimlim <macalimlim@gmail.com>
2013-09-07 13:54:32 +0800

    removed interface

--- src/net/dlogic/kryonet/server/event/handler/ILoginEventHandler.java
+++
@@ -1,9 +0,0 @@
-package net.dlogic.kryonet.server.event.handler;
-
-import com.esotericsoftware.kryonet.Connection;
-
-public interface ILoginEventHandler extends IEventHandler {
-	public void onLogin(Connection connection, String username, String password);
-	public void sendLoginSuccess();
-	public void sendLoginFailure();
-}
