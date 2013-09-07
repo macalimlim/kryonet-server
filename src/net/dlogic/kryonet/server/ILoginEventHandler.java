8ca96b12590c45054f6b0580fcd94ffea7414c88
macalimlim <macalimlim@gmail.com>
2013-09-07 11:20:50 +0800

    moved class file to another package

--- src/net/dlogic/kryonet/server/ILoginEventHandler.java
+++
@@ -1,7 +0,0 @@
-package net.dlogic.kryonet.server;
-
-import com.esotericsoftware.kryonet.Connection;
-
-public interface ILoginEventHandler {
-	public void onLogin(Connection connection, String username, String password);
-}
