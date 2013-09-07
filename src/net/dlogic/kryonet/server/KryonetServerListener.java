60be436cc2b66242e5e3ffcec83ee741f44d9bbc
macalimlim <macalimlim@gmail.com>
2013-09-07 16:45:12 +0800

    added a handler for LogoutRequest

--- src/net/dlogic/kryonet/server/KryonetServerListener.java
+++ src/net/dlogic/kryonet/server/KryonetServerListener.java
@@ -2,21 +2,27 @@ package net.dlogic.kryonet.server;
 
 import net.dlogic.kryonet.common.request.JoinRoomRequest;
 import net.dlogic.kryonet.common.request.LoginRequest;
+import net.dlogic.kryonet.common.request.LogoutRequest;
 import net.dlogic.kryonet.server.event.handler.JoinRoomEventHandler;
 import net.dlogic.kryonet.server.event.handler.LoginEventHandler;
+import net.dlogic.kryonet.server.event.handler.LogoutEventHandler;
 
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.reflectasm.ConstructorAccess;
 
 public class KryonetServerListener extends Listener {
-	private Class<? extends LoginEventHandler> loginEventHandler;
 	private Class<? extends JoinRoomEventHandler> joinRoomEventHandler;
+	private Class<? extends LoginEventHandler> loginEventHandler;
+	private Class<? extends LogoutEventHandler> logoutEventHandler;
+	public void setJoinRoomEventHandler(Class<? extends JoinRoomEventHandler> handler) {
+		joinRoomEventHandler = handler;
+	}
 	public void setLoginEventHandler(Class<? extends LoginEventHandler> handler) {
 		loginEventHandler = handler;
 	}
-	public void setJoinRoomEventHandler(Class<? extends JoinRoomEventHandler> handler) {
-		joinRoomEventHandler = handler;
+	public void setLogoutEventHandler(Class<? extends LogoutEventHandler> handler) {
+		logoutEventHandler = handler;
 	}
 	public void connected(Connection connection) {
 		// TODO Auto-generated method stub
@@ -34,12 +40,18 @@ public class KryonetServerListener extends Listener {
 			handler.setConnection(connection);
 			handler.onJoinRoom(request.roomToJoin, request.password);
 		} else if (object instanceof LoginRequest) {
-			LoginRequest reuqest = (LoginRequest)object;
+			LoginRequest request = (LoginRequest)object;
 			ConstructorAccess<? extends LoginEventHandler> access = ConstructorAccess.get(loginEventHandler);
 			LoginEventHandler handler = access.newInstance();
 			handler.setConnection(connection);
-			handler.onLogin(reuqest.username, reuqest.password);
-		} 
+			handler.onLogin(request.username, request.password);
+		} else if (object instanceof LogoutRequest) {
+			LogoutRequest request = (LogoutRequest)object;
+			ConstructorAccess<? extends LogoutEventHandler> access = ConstructorAccess.get(logoutEventHandler);
+			LogoutEventHandler handler = access.newInstance();
+			handler.setConnection(connection);
+			handler.onLogout(request.userLoggingOut);
+		}
 	}
 	public void idle(Connection connection) {
 		// TODO Auto-generated method stub
