c548583fee57fee183f4acf929f01b421e11a8d9
macalimlim <macalimlim@gmail.com>
2013-09-07 08:48:55 +0800

    added implementation for handling incoming login requests

--- src/net/dlogic/kryonet/server/KryonetServerListener.java
+++ src/net/dlogic/kryonet/server/KryonetServerListener.java
@@ -1,9 +1,17 @@
 package net.dlogic.kryonet.server;
 
+import net.dlogic.kryonet.common.request.LoginRequest;
+
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
+import com.esotericsoftware.reflectasm.ConstructorAccess;
+import com.esotericsoftware.reflectasm.MethodAccess;
 
 public class KryonetServerListener extends Listener {
+	private Class<ILoginEventHandler> loginEventHandler;
+	public void setLoginEventHandler(Class<ILoginEventHandler> handler) {
+		loginEventHandler = handler;
+	}
 	public void connected(Connection connection) {
 		// TODO Auto-generated method stub
 		super.connected(connection);
@@ -13,8 +21,12 @@ public class KryonetServerListener extends Listener {
 		super.disconnected(connection);
 	}
 	public void received(Connection connection, Object object) {
-		// TODO Auto-generated method stub
-		super.received(connection, object);
+		if (object instanceof LoginRequest) {
+			LoginRequest loginRequest = (LoginRequest)object;
+			ConstructorAccess<ILoginEventHandler> access = ConstructorAccess.get(loginEventHandler);
+			ILoginEventHandler handler = access.newInstance();
+			handler.onLogin(connection, loginRequest.username, loginRequest.password);
+		}
 	}
 	public void idle(Connection connection) {
 		// TODO Auto-generated method stub
