65896c66450d8e30b3122bfceb24091aa7a98027
macalimlim <macalimlim@gmail.com>
2013-09-09 21:46:29 +0800

    modified sendJoinRoomSuccessResponse to send the response to every user in the room. modified the sendJoinRoomFailure to send the error back to the sender

--- src/net/dlogic/kryonet/server/event/handler/JoinRoomEventHandler.java
+++ src/net/dlogic/kryonet/server/event/handler/JoinRoomEventHandler.java
@@ -1,6 +1,9 @@
 package net.dlogic.kryonet.server.event.handler;
 
+import java.util.Iterator;
+
 import net.dlogic.kryonet.common.entity.Room;
+import net.dlogic.kryonet.common.entity.User;
 import net.dlogic.kryonet.common.response.JoinRoomFailureResponse;
 import net.dlogic.kryonet.common.response.JoinRoomSuccessResponse;
 
@@ -9,11 +12,14 @@ public abstract class JoinRoomEventHandler extends BaseEventHandler {
 	public void sendJoinRoomSuccessResponse(Room joinedRoom) {
 		JoinRoomSuccessResponse response = new JoinRoomSuccessResponse();
 		response.joinedRoom = joinedRoom;
-		connection.sendTCP(response);
+		Iterator<User> it = joinedRoom.getUserList().iterator();
+		while (it.hasNext()) {
+			it.next().getConnection().sendTCP(response);
+		}
 	}
 	public void sendJoinRoomFailureResponse(String errorMessage) {
 		JoinRoomFailureResponse response = new JoinRoomFailureResponse();
 		response.errorMessage = errorMessage;
-		connection.sendTCP(response);
+		sender.getConnection().sendTCP(response);
 	}
 }
