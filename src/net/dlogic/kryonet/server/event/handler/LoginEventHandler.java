0133b69966f4d2ec0b234d37005cca1946aa88cf
macalimlim <macalimlim@gmail.com>
2013-09-09 21:50:17 +0800

    modified methods to send back the success or failure of the login

--- src/net/dlogic/kryonet/server/event/handler/LoginEventHandler.java
+++ src/net/dlogic/kryonet/server/event/handler/LoginEventHandler.java
@@ -1,19 +1,19 @@
 package net.dlogic.kryonet.server.event.handler;
 
-import net.dlogic.kryonet.common.entity.User;
+import net.dlogic.kryonet.common.exception.LoginException;
 import net.dlogic.kryonet.common.response.LoginFailureResponse;
 import net.dlogic.kryonet.common.response.LoginSuccessResponse;
 
 public abstract class LoginEventHandler extends BaseEventHandler {
-	public abstract void onLogin(String username, String password);
-	public final void sendLoginSuccessResponse(User user) {
+	public abstract void onLogin(String username, String password) throws LoginException;
+	public final void sendLoginSuccessResponse() {
 		LoginSuccessResponse response = new LoginSuccessResponse();
-		response.myself = user;
-		connection.sendTCP(response);
+		response.myself = sender;
+		sender.getConnection().sendTCP(response);
 	}
 	public final void sendLoginFailureResponse(String errorMessage) {
 		LoginFailureResponse response = new LoginFailureResponse();
 		response.errorMessage = errorMessage;
-		connection.sendTCP(response);
+		sender.getConnection().sendTCP(response);
 	}
 }
