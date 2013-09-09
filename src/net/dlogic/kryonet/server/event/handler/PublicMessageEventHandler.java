463b54cfa4bffef9cc537d613f507f0de74607ad
macalimlim <macalimlim@gmail.com>
2013-09-09 21:44:28 +0800

    modified sendPublicMessageResponse method to send to every user in the room

--- src/net/dlogic/kryonet/server/event/handler/PublicMessageEventHandler.java
+++ src/net/dlogic/kryonet/server/event/handler/PublicMessageEventHandler.java
@@ -1,15 +1,20 @@
 package net.dlogic.kryonet.server.event.handler;
 
+import java.util.Iterator;
+
 import net.dlogic.kryonet.common.entity.Room;
 import net.dlogic.kryonet.common.entity.User;
 import net.dlogic.kryonet.common.response.PublicMessageResponse;
 
 public abstract class PublicMessageEventHandler extends BaseEventHandler {
 	public abstract void onPublicMessage(String message, Room targetRoom);
-	public void sendPublicMessageResponse(String message, User fromUser) {
+	public void sendPublicMessageResponse(String message, User fromUser, Room targetRoom) {
 		PublicMessageResponse response = new PublicMessageResponse();
 		response.fromUser = fromUser;
 		response.message = message;
-		connection.sendTCP(response);
+		Iterator<User> it = targetRoom.getUserList().iterator();
+		while (it.hasNext()) {
+			it.next().getConnection().sendTCP(response);
+		}
 	}
 }
