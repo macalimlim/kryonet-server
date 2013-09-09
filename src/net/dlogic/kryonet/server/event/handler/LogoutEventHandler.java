5172cca44755ac5c2edfed057a07540dc8e17170
macalimlim <macalimlim@gmail.com>
2013-09-09 21:47:03 +0800

    modified method to send the LogoutResponse back to the sender

--- src/net/dlogic/kryonet/server/event/handler/LogoutEventHandler.java
+++ src/net/dlogic/kryonet/server/event/handler/LogoutEventHandler.java
@@ -5,9 +5,9 @@ import net.dlogic.kryonet.common.response.LogoutResponse;
 
 public abstract class LogoutEventHandler extends BaseEventHandler {
 	public abstract void onLogout(User userLoggingOut);
-	public void sendLogoutResponse(User userLoggingOut) {
+	public void sendLogoutResponse() {
 		LogoutResponse response = new LogoutResponse();
-		response.userLoggingOut = userLoggingOut;
-		connection.sendTCP(response);
+		response.userLoggingOut = sender;
+		sender.getConnection().sendTCP(response);
 	}
 }
