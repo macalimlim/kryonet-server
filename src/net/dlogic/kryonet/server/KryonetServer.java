26d0e16d0cbe270a7068050952a6051f301debea
macalimlim <macalimlim@gmail.com>
2013-09-12 01:37:08 +0800

    refactored KryonetServer class

--- src/net/dlogic/kryonet/server/KryonetServer.java
+++ src/net/dlogic/kryonet/server/KryonetServer.java
@@ -4,15 +4,28 @@ import java.io.IOException;
 
 import net.dlogic.kryonet.common.utility.KryonetUtility;
 
+import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.kryonet.Server;
 
 public class KryonetServer {
-	private static Server server;
-	public KryonetServer(int writeBufferSize, int objectBufferSize, int tcpPort, int udpPort) throws IOException {
+	private Server server;
+	private KryonetServerListener listener;
+	public KryonetServer(int writeBufferSize, int objectBufferSize) {
 		server = new Server(writeBufferSize, objectBufferSize);
 		KryonetUtility.registerClasses(server);
+		server.addListener(listener = new KryonetServerListener());
+	}
+	public void start(int tcpPort, int udpPort) throws IOException {
 		server.start();
 		server.bind(tcpPort, udpPort);
-		server.addListener(new KryonetServerListener());
+	}
+	public KryonetServerListener getKryonetServerListener() {
+		return listener;
+	}
+	public void addListener(Listener listener) {
+		server.addListener(listener);
+	}
+	public void registerClass(Class type) {
+		server.getKryo().register(type);
 	}
 }
