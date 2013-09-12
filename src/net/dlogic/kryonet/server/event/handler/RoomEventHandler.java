3b1d76b03b17260cc86a213069a4420c67428cad
macalimlim <macalimlim@gmail.com>
2013-09-12 09:36:42 +0800

    added throws JoinRoomException to the onJoinRoom() method

--- src/net/dlogic/kryonet/server/event/handler/RoomEventHandler.java
+++ src/net/dlogic/kryonet/server/event/handler/RoomEventHandler.java
@@ -2,8 +2,9 @@ package net.dlogic.kryonet.server.event.handler;
 
 import net.dlogic.kryonet.common.entity.Room;
 import net.dlogic.kryonet.common.entity.User;
+import net.dlogic.kryonet.common.exception.JoinRoomException;
 
 public abstract class RoomEventHandler extends BaseEventHandler {
-	public abstract void onJoinRoom(Room roomToJoin, String password);
+	public abstract void onJoinRoom(Room roomToJoin, String password) throws JoinRoomException;
 	public abstract void onLeaveRoom(User userToLeave, Room roomToLeave);
 }
