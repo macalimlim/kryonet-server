883670ab99b0bf418595d5b93ba99fd8986c8650
macalimlim <macalimlim@gmail.com>
2013-09-13 14:38:37 +0800

    refactored event handlers

--- src/net/dlogic/kryonet/server/event/handler/PersonMessageEventHandler.java
+++ src/net/dlogic/kryonet/server/event/handler/PersonMessageEventHandler.java
@@ -4,6 +4,6 @@ import net.dlogic.kryonet.common.entity.Room;
 import net.dlogic.kryonet.common.entity.User;
 
 public abstract class PersonMessageEventHandler extends BaseEventHandler {
-	public abstract void onPrivateMessage(String message, User targetUser);
-	public abstract void onPublicMessage(String message, Room targetRoom);
+	public abstract void onPrivateMessage(User targetUser, String message);
+	public abstract void onPublicMessage(Room targetRoom, String message);
 }
--- src/net/dlogic/kryonet/server/event/handler/RoomEventHandler.java
+++ src/net/dlogic/kryonet/server/event/handler/RoomEventHandler.java
@@ -6,5 +6,5 @@ import net.dlogic.kryonet.common.exception.JoinRoomException;
 
 public abstract class RoomEventHandler extends BaseEventHandler {
 	public abstract void onJoinRoom(Room roomToJoin, String password) throws JoinRoomException;
-	public abstract void onLeaveRoom(User userToLeave, Room roomToLeave);
+	public abstract void onLeaveRoom(Room roomToLeave);
 }
