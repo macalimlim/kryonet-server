1e961179b08dcca6b891aea0f4ee310f8bd87c4d
macalimlim <macalimlim@gmail.com>
2013-09-13 18:37:26 +0800

    added logging

--- src/net/dlogic/kryonet/server/KryonetServer.java
+++ src/net/dlogic/kryonet/server/KryonetServer.java
@@ -6,16 +6,19 @@ import net.dlogic.kryonet.common.utility.KryonetUtility;
 
 import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.kryonet.Server;
+import com.esotericsoftware.minlog.Log;
 
 public class KryonetServer {
 	private Server server;
 	private KryonetServerListener listener;
 	public KryonetServer(int writeBufferSize, int objectBufferSize) {
+		Log.info("KryonetServer(" + writeBufferSize +  ", " + objectBufferSize + ")");
 		server = new Server(writeBufferSize, objectBufferSize);
 		KryonetUtility.registerClasses(server);
 		server.addListener(listener = new KryonetServerListener(server));
 	}
 	public void start(int tcpPort, int udpPort) throws IOException {
+		Log.info("KryonetServer.start(" + tcpPort +  ", " + udpPort + ")");
 		server.start();
 		server.bind(tcpPort, udpPort);
 	}
