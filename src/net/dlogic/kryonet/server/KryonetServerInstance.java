99f292570d9c0a383552e99afe6682d950f8d118
macalimlim <macalimlim@gmail.com>
2013-09-13 18:33:10 +0800

    added logging

--- src/net/dlogic/kryonet/server/KryonetServerInstance.java
+++ src/net/dlogic/kryonet/server/KryonetServerInstance.java
@@ -2,15 +2,19 @@ package net.dlogic.kryonet.server;
 
 import java.io.IOException;
 
+import com.esotericsoftware.minlog.Log;
+
 public class KryonetServerInstance {
 	private static KryonetServer server;
 	public static void initialize(int writeBufferSize, int objectBufferSize) throws IOException, KryonetServerException {
+		Log.info("KryonetServerInstance.initialize(" + writeBufferSize + ", " + objectBufferSize + ")");
 		if (server != null) {
 			throw new KryonetServerException(KryonetServerException.ALREADY_INITIALIZED);
 		}
 		server = new KryonetServer(writeBufferSize, objectBufferSize);
 	}
 	public static KryonetServer getInstance() throws KryonetServerException {
+		Log.info("KryonetServerInstance.getInstance()");
 		if (server == null) {
 			throw new KryonetServerException(KryonetServerException.NOT_INITIALIZED);
 		}
