ce6d3fed3a2655233c4df401f39c32fd02fc12d3
macalimlim <macalimlim@gmail.com>
2013-09-15 07:24:35 +0800

    implemented the onConnected method

--- app/MyConnectionEventHandler.java
+++ app/MyConnectionEventHandler.java
@@ -4,7 +4,13 @@ import net.dlogic.kryonet.server.event.handler.ConnectionEventHandler;
 public class MyConnectionEventHandler extends ConnectionEventHandler {
 
 	@Override
-	public void onDisconnect() {
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onConnected() {
 		// TODO Auto-generated method stub
 		
 	}
