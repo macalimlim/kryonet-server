254ddcf6d9cc29a9b97fc8c6c89b6cf258ceba0e
macalimlim <macalimlim@gmail.com>
2013-09-15 02:51:34 +0800

    added MyConnectionEventHandler class for running/testing

---
+++ app/MyConnectionEventHandler.java
@@ -0,0 +1,12 @@
+import net.dlogic.kryonet.server.event.handler.ConnectionEventHandler;
+
+
+public class MyConnectionEventHandler extends ConnectionEventHandler {
+
+	@Override
+	public void onDisconnect() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
