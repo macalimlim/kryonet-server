2f7872d784296237e2f3435654c8336531403411
macalimlim <macalimlim@gmail.com>
2013-09-15 07:22:32 +0800

    added onConnected abstract method

--- src/net/dlogic/kryonet/server/event/handler/ConnectionEventHandler.java
+++ src/net/dlogic/kryonet/server/event/handler/ConnectionEventHandler.java
@@ -1,5 +1,6 @@
 package net.dlogic.kryonet.server.event.handler;
 
 public abstract class ConnectionEventHandler extends BaseEventHandler {
-	public abstract void onDisconnect();
+	public abstract void onConnected();
+	public abstract void onDisconnected();
 }
