3fe8a6edfac9d19e67ad4a92d26ea93fa8e8f511
macalimlim <macalimlim@gmail.com>
2013-09-15 07:22:09 +0800

    made the Server and KryonetServerListener objects public and removed unnecessary methods

--- src/net/dlogic/kryonet/server/KryonetServer.java
+++ src/net/dlogic/kryonet/server/KryonetServer.java
@@ -4,34 +4,21 @@ import java.io.IOException;
 
 import net.dlogic.kryonet.common.utility.KryonetUtility;
 
-import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.kryonet.Server;
 import com.esotericsoftware.minlog.Log;
 
 public class KryonetServer {
-	private Server server;
-	private KryonetServerListener listener;
+	public Server endpoint;
+	public KryonetServerListener listener;
 	public KryonetServer(int writeBufferSize, int objectBufferSize) {
 		Log.info("KryonetServer(" + writeBufferSize +  ", " + objectBufferSize + ")");
-		server = new Server(writeBufferSize, objectBufferSize);
-		KryonetUtility.registerClasses(server);
-		server.addListener(listener = new KryonetServerListener());
+		endpoint = new Server(writeBufferSize, objectBufferSize);
+		KryonetUtility.registerClasses(endpoint);
+		endpoint.addListener(listener = new KryonetServerListener());
 	}
 	public void start(int tcpPort, int udpPort) throws IOException {
 		Log.info("KryonetServer.start(" + tcpPort +  ", " + udpPort + ")");
-		server.start();
-		server.bind(tcpPort, udpPort);
-	}
-	public KryonetServerListener getKryonetServerListener() {
-		return listener;
-	}
-	public void addListener(Listener listener) {
-		server.addListener(listener);
-	}
-	public void registerClass(Class type) {
-		server.getKryo().register(type);
-	}
-	public Server getServer() {
-		return server;
+		endpoint.start();
+		endpoint.bind(tcpPort, udpPort);
 	}
 }
