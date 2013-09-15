d2de8cb36093dca3de2a133240b59a9e5fd3a248
macalimlim <macalimlim@gmail.com>
2013-09-15 09:44:22 +0800

    deleted class file replaced by UserEventHandler class

--- src/net/dlogic/kryonet/server/event/handler/LoginOrLogoutEventHandler.java
+++
@@ -1,8 +0,0 @@
-package net.dlogic.kryonet.server.event.handler;
-
-import net.dlogic.kryonet.common.exception.LoginException;
-
-public abstract class LoginOrLogoutEventHandler extends BaseEventHandler {
-	public abstract void onLogin(String username, String password) throws LoginException;
-	public abstract void onLogout();
-}
