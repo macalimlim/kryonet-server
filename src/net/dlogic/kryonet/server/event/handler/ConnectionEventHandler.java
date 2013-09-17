68165060a5d95083c9bf9ecc854ecd8ac2f41b08
macalimlim <macalimlim@gmail.com>
2013-09-17 09:32:51 +0800

    added onIdle method

--- src/net/dlogic/kryonet/server/event/handler/ConnectionEventHandler.java
+++ src/net/dlogic/kryonet/server/event/handler/ConnectionEventHandler.java
@@ -3,4 +3,5 @@ package net.dlogic.kryonet.server.event.handler;
 public abstract class ConnectionEventHandler extends BaseEventHandler {
 	public abstract void onConnected();
 	public abstract void onDisconnected();
+	public abstract void onIdle();
 }
